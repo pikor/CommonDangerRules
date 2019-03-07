50f0b3ef3bed5a7bf68beac5d5d328446d1ffccf
Lukasz Pikor <vonpik@gmail.com>
2019-03-07 17:34:27 +0100

    Initial commit.

---
+++ Dangerfile.swift
@@ -0,0 +1,8 @@
+import Danger
+
+let danger = Danger()
+
+public func doCodeReview() -> Void {
+    let editedFiles = danger.git.modifiedFiles + danger.git.createdFiles
+    message("These files have changed: \(editedFiles.joined())")
+}
---
+++ Tests/LinuxMain.swift
@@ -0,0 +1,7 @@
+import XCTest
+
+import CommonDangerRulesTests
+
+var tests = [XCTestCaseEntry]()
+tests += CommonDangerRulesTests.allTests()
+XCTMain(tests)
