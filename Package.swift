12d45dcdb1205bc09e8fc32c7c87914d00699f71
Lukasz Pikor <vonpik@gmail.com>
2019-03-08 10:06:24 +0100

    Move main source file to proper place.

--- Package.swift
+++ Package.swift
@@ -18,8 +18,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "CommonDangerRules",
-            dependencies: ["Danger"],
-            path: "Sources"),
+            dependencies: ["Danger"]),
         .testTarget(
             name: "CommonDangerRulesTests",
             dependencies: ["CommonDangerRules"]),
--- Sources/CommonDangerRules/CommonDangerRules.swift
+++ Sources/CommonDangerRules/CommonDangerRules.swift
@@ -3,7 +3,7 @@ import Danger
 let danger = Danger()
 
 final class CommonDangerRules {
-
+    
     public static func doCodeReview() -> Void {
         let editedFiles = danger.git.modifiedFiles + danger.git.createdFiles
         message("These files have changed: \(editedFiles.joined())")
